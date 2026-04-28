70a14f4f93a5bc67b62efe71a8525da94294100d
Keigo Nitadori <keigo@riken.jp>
2026-04-28 16:12:01 +0900

    除算削減

--- srcomp_cpp/mhd.cpp
+++ srcomp_cpp/mhd.cpp
@@ -1692,10 +1692,13 @@ void UpdateConservU(const GridArray<double>& G,const FieldArray<double>& Fx,cons
 	Kokkos::parallel_for("UpdateConservU",
 	Kokkos::MDRangePolicy<Kokkos::Rank<3>>({is, js, ks}, {ie+1, je+1, ke+1}),
 	KOKKOS_LAMBDA(const int i, const int j, const int k) {
+		const double xinv = 1.0 / (G.x1a(i+1)-G.x1a(i));
+		const double yinv = 1.0 / (G.x2a(i+1)-G.x2a(i));
+		const double zinv = 1.0 / (G.x3a(i+1)-G.x3a(i));
 		for (int m=0; m<mconsv; m++) {
-			U.href(m,k,j,i) -= dt * ( (Fx(m,k,j,i+1) - Fx(m,k,j,i)) / (G.x1a(i+1)-G.x1a(i))
-			                         +(Fy(m,k,j+1,i) - Fy(m,k,j,i)) / (G.x2a(j+1)-G.x2a(j))
-			                         +(Fz(m,k+1,j,i) - Fz(m,k,j,i)) / (G.x3a(k+1)-G.x3a(k)) );
+			U.href(m,k,j,i) -= dt * ( (Fx(m,k,j,i+1) - Fx(m,k,j,i)) * xinv
+			                         +(Fy(m,k,j+1,i) - Fy(m,k,j,i)) * yinv
+			                         +(Fz(m,k+1,j,i) - Fz(m,k,j,i)) * zinv );
 		}
 	});
 #endif
